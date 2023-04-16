44716a721f63c72874dc515d0603a5cf868b827a
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-04-16 13:49:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const ul = document.getElementsByTagName("ul")[0];
+let displayText="The level of the element is: "
+for(let n=0;n<ul.children.length;n++){
+  if(ul.children[n].id==="level")
+    alert(displayText+(n+1));
+}
